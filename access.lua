e1cf2d46e6b49ad8bc71108daf5ea89ec9cea6a1
XspeedHub <eeuuleyenda@gmail.com>
2026-03-18 14:33:10 -0600

    Update access.lua

--- access.lua
+++ access.lua
@@ -2,7 +2,7 @@ return {
     FREE_DAY = false,
     ALLOWED_USERS = {
         "N4rczis0",
-        "XxsebasxX9009" -- 1 hora empieza 2:24 termina 3:24
+        "XxsebasxX9009", -- 1 hora empieza 2:24 termina 3:24
         "24kboi_mo", -- publicidad
         "Jhon1432",
         "ZL",
