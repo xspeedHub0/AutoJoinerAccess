6885660443c851b7601aaeb9279e65df79ce5981
XspeedHub <eeuuleyenda@gmail.com>
2026-03-18 09:32:56 -0600

    Update access.lua

--- access.lua
+++ access.lua
@@ -2,7 +2,7 @@ return {
     FREE_DAY = false,
     ALLOWED_USERS = {
         "N4rczis0",
-        "24kboi_mo" -- publicidad
+        "24kboi_mo", -- publicidad
         "Jhon1432",
         "ZL",
     }
